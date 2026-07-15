4f7ce9c343fb343913421659b1947dbaf6f0b8bc
mauriciocasas0804-ops <mauricio.casas.08.04@gmail.com>
2026-07-15 01:37:06 -0600

    Update stock.js

--- src/stock.js
+++ src/stock.js
@@ -1,1 +1,17 @@
+export async function getStock() {
+  // Aquí después conectaremos la API o fuente de datos del stock
 
+  const stock = [
+    "Rocket",
+    "Spin",
+    "Flame",
+    "Ice"
+  ];
+
+  return stock;
+}
+
+
+export function searchFruit(stock, fruitName) {
+  return stock.includes(fruitName);
+}
