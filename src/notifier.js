5439cbb0e218a9f3bd93c7b8064d9418edaf2528
mauriciocasas0804-ops <mauricio.casas.08.04@gmail.com>
2026-07-15 02:32:27 -0600

    Update notifier.js

--- src/notifier.js
+++ src/notifier.js
@@ -1,4 +1,29 @@
-export function sendNotification(message) {
-  console.log("🔔 NOTIFICACIÓN:");
-  console.log(message);
+const webhookURL = process.env.DISCORD_WEBHOOK;
+
+
+export async function sendNotification(message) {
+  if (!webhookURL) {
+    console.error("No existe el webhook de Discord");
+    return;
+  }
+
+  try {
+    await fetch(webhookURL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        content: message,
+      }),
+    });
+
+    console.log("✅ Notificación enviada a Discord");
+
+  } catch (error) {
+    console.error(
+      "Error enviando notificación:",
+      error.message
+    );
+  }
 }
